ea9f6e3a13e581d1a959ef27e8bd9f277e43d004
kenji suzuki <xkenji.suzuki@gmail.com>
2018-12-31 21:39:38 +0900

    Add install-docker script for Google Compute Engine.

--- helpers/install-docker-on-gce.sh
+++ helpers/install-docker-on-gce.sh
@@ -16,3 +16,8 @@ add-apt-repository \
 # install docker
 apt-get update
 apt-get install -y docker-ce
+
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" \
+   -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose

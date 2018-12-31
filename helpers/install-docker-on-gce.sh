1c3defc83d85ab19e30e8190e932dff1753a51fb
kenji suzuki <xkenji.suzuki@gmail.com>
2018-12-31 21:51:25 +0900

    Add sudo to install script.

--- helpers/install-docker-on-gce.sh
+++ helpers/install-docker-on-gce.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 # setup the repository
-apt-get update
-apt-get install -y \
+sudo apt-get update
+sudo apt-get install -y \
    apt-transport-https \
    ca-certificates \
    curl \
    gnupg2 \
    software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-apt-key fingerprint 0EBFCD88
-add-apt-repository \
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
 # install docker
-apt-get update
-apt-get install -y docker-ce
-
+sudo apt-get update
+sudo apt-get install -y docker-ce --allow-unauthenticated
 # install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" \
    -o /usr/local/bin/docker-compose
